b78c42ea4a1bdb95eb577789f959b8ac2ec412bd
kamil kawa <kamil.kawa@gmail.com>
2016-04-09 18:07:09 +0000

    disabling passport for heroku

--- server.js
+++ server.js
@@ -25,8 +25,8 @@ app.use(session({
 
 
 api(app);
-app.use(passport.initialize());
-app.use(passport.session());
+//app.use(passport.initialize());
+//app.use(passport.session());
 
 routes(app, passport);
 
